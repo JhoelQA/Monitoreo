ebb7cc28850213fcd419fd1ba8d09f0233caa982
JhoelQA <jhoel.quispe@ib.edu.ar>
2023-09-29 15:10:47 -0300

    Ploteo de datos del acelerómetro

---
+++ lectura de datos.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def graficar_acel(v):
+  file = open("C:\\Users\\Win\\Documents\\Arduino\\PI_1\\acelerómetro ODR = "+ str(v)+"Hz.txt", 'r')
+  #file = open("acelerómetro ODR = "+ str(v)+"Hz.txt", 'r')
+  a_x = []
+  a_y = []
+  a_z = []
+  i = 0
+  for line in file:
+      #aux = line
+      aux = line.replace("\n",'')
+      aux = aux.split('\t')
+      #print(aux)
+      try:
+        a_x.append(float(aux[0]))
+        a_y.append(float(aux[1]))
+        a_z.append(float(aux[2]))
+      except ValueError:
+        print("error","on line",i)
+      i = i+1
+  file.close()
+  t = np.linspace(0, 20, len(a_x))
+  #plt.figure()
+  plt.plot(t, a_x)
+  plt.plot(t, a_y)
+  plt.plot(t, a_z)
+  plt.title("ODR = "+ str(v) + " Hz")
+  plt.xlabel("Tiempo [s]")
+  plt.ylabel("Aceleración [g]")
+  plt.legend(["a_x", "a_y", "a_z"])
+  plt.grid()
+  plt.show()
+
+graficar_acel(952) 
+# freq = [10, 50, 119, 238, 476, 952]
+# for i in freq:
+#   graficar_acel(i) 
