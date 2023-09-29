c710f0e2c3d5826ce2171a15e01a75e2b83661c1
JhoelQA <jhoel.quispe@ib.edu.ar>
2023-09-29 14:43:50 -0300

    uso

--- comu_with_python.py
+++ comu_with_python.py
@@ -1,7 +1,7 @@
 #Prueba de git
 import serial
 import time
-archivo = open("C:\\Users\\Win\\Documents\\Arduino\\PI_1\\datos_accel2.txt",'w')
+archivo = open("C:\\Users\\Win\\Documents\\Arduino\\PI_1\\acelerómetro ODR = 952Hz.txt",'w')
 serialArduino = serial.Serial('COM4', 9600)
 time.sleep(1)
 start_time = time.time()
@@ -9,7 +9,7 @@ while True:
     datos = serialArduino.readline().decode("ascii")
     print(datos)
     archivo.write(datos)
-    if(time.time() - start_time > 10):
+    if(time.time() - start_time > 20):
         print("Conexión terminada")
         break
 
